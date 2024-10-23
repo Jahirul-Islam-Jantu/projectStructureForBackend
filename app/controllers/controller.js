cd4323c02c2f6aa72c5a487ee1c9066ee34a23f3
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-23 21:54:47 +0600

    structure

---
+++ app/controllers/controller.js
@@ -0,0 +1,12 @@
+export const createController = async (req, res)=> {
+    return res.json({message:"created successfully"})
+}
+export const readController = async (req, res)=> {
+    return res.json({message:"read successfully"})
+}
+export const updateController = async (req, res)=> {
+    return res.json({message:"Updated successfully"})
+}
+export const deleteController = async (req, res)=> {
+    return res.json({message:"deleted successfully"})
+}
