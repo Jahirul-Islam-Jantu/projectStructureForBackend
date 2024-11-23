1d0911efae93782c1e51a14fb68350169d24150d
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-11-23 18:43:08 +0600

    get all users

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -15,3 +15,18 @@ export const CreateUser = async (req, res) => {
     res.status(400).json({ message: "User create failed" });
   }
 };
+
+export const getUsers = async (req, res) => {
+  try {
+    let result = await UserModel.find();
+    if (result) {
+      res
+        .status(201)
+        .json({ message: "User create successfully", user: result });
+    } else {
+      res.status(400).json({ message: "User create failed" });
+    }
+  } catch (e) {
+    res.status(400).json({ message: "User create failed" });
+  }
+};
