1c1c43b55da1ebfb759dbeeaf746d21894c6fb9c
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-11-25 22:06:11 +0600

    find single user

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 const UserMiddleWare = (req, res, next) => {
+  let email = req.headers["email"];
   let id = req.params.id;
-  if (!id) {
+  if (!id || !email) {
     res.status(400).json({ message: "User not found" });
   } else if (!mongoose.Types.ObjectId.isValid(id)) {
     res.status(400).json({ message: "invalid Id" });
   }
+  req.email = email;
   req._id = id;
   next();
 };
